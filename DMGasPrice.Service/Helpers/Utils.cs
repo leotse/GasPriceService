bc3406071353e877c163b8d6dad78d5d5259c893
Leo Tse <leo.tse@digiflare.com>
2011-07-25 02:07:04 -0400

    changed date parsing fallback behaviour

--- DMGasPrice.Service/Helpers/Utils.cs
+++ DMGasPrice.Service/Helpers/Utils.cs
@@ -25,7 +25,7 @@ namespace DMGasPrice.Service.Helpers
                 }
             }
 
-            return DateTime.Today.AddDays(1);
+            return DateTime.Today;
         }
 
         public static int ExtractCityKey(string text)
